3117e20c8c07b5924d2b1aafc1507b25d8e6f5fd
Roxie Linden <roxie@lindenlab.com>
2023-05-24 23:18:00 +0000

    Increase depth test

--- tests/bench.py
+++ tests/bench.py
@@ -85,7 +85,7 @@ def build_deep_xml():
 
     deep_data = {}
     curr_data = deep_data
-    for i in range(250):
+    for i in range(1000):
         curr_data["curr_data"] = {}
         curr_data["integer"] = 7
         curr_data["string"] = "string"
